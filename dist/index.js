7ba175af7774f36c7a19af3c058e786bc22bc2f6
Alexander Bogoslawski <Alexander.bogoslawski@web.de>
2016-04-16 22:56:43 +0200

    a

--- dist/index.js
+++ dist/index.js
@@ -6,9 +6,9 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _nominatimGeocode = require('nominatim-geocode2');
+var _nodeNominatim = require('node-nominatim2');
 
-var _nominatimGeocode2 = _interopRequireDefault(_nominatimGeocode);
+var _nodeNominatim2 = _interopRequireDefault(_nodeNominatim);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -18,7 +18,7 @@ var _class = function () {
     function _class(p) {
         _classCallCheck(this, _class);
 
-        this.n = new _nominatimGeocode2.default(p);
+        this.n = new _nodeNominatim2.default(p);
     }
 
     _createClass(_class, [{
